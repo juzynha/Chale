5c9d9b85f4a84162d795d1250f35c5a47bdbd7c5
Juliana Cardoso Araujo <julianacaraujo3103@gmail.com>
2025-04-01 20:51:36 -0300

    primeiro commit

---
+++ app/view/cabecalho.php
@@ -0,0 +1,15 @@
+<?php
+require 'header.html';
+?>
+<div>
+    <img src="../public/assets/imagens/logo.svg" alt="">
+    <ul>
+        <li><a href="">O Chalé</a></li>
+        <li><a href="">Sobre nós</a></li>
+        <li><a href="">Faça sua Reserva</a></li>
+    </ul>
+    <button>Entrar</button>
+</div>
+<?php
+require 'footer.html';
+?>
