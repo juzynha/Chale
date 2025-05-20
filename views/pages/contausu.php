c9e89e01d4970230d5796d5f66dbc06f315ec337
Marina <marinapassara125@gmail.com>
2025-05-19 22:25:48 -0300

    Pequenas alterações na página conta de usuário

--- views/pages/contausu.php
+++ views/pages/contausu.php
@@ -5,59 +5,54 @@ require_once __DIR__ . '/../layouts/header.php';
     <?php
     require_once __DIR__ . '/../layouts/menu.php';
     ?>
-    
-
-    <div class="icone-perfil-cabecalho">
-        <img src="/chale/public/assets/icon-user.svg" alt="ícone do perfil" width="40" height="40">
-    </div>
 </header>
-
 <section class="perfil">
     <div class="usuario">
-        <img src="/chale/public/assets/icon-user.svg" alt="avatar do usuario" width="100" height="100">
+        <img src="/chale/public/assets/icons/icon-user.svg" alt="avatar do usuario" width="100"   height="100">
         <div>
-            <h2> nome: Gabriel Cardoso </h2>
-            <h3> email: Gabriel.cardoso@gmail.com </h3>
-            <h3> telefone: (11) 99891-0191 </h3>
-            <h3> Data de nascimento: 01/01/2000 </h3>
-        </div>
-    </div>
-</section>
+<h2> Gabriel cardoso <h2>
+<h3> Email:Gabriel.cardoso@gmail.com <h3>
+<h3> Telefone:(14)9908-9303</h3>    
+<h3> Data de nascimento:04/11/2020</h3>
+  </div>
+</div>
 
-
-
-
-<div class="acoes">
-    <h3> Ações </h3>
+<div class="reservas">
+    <h3> Reservas </h3>
     <ul>
-        <li><a herf="#">À pagar</a></li>
-        <li><a herf="#">Em andamento</a></li>
-        
-        <h3>Caso queira cancelar uma reserva, entre em contato com o anfitrião.</h3>     
+        <li><a href="#">À pagar</a></li>
+        <li><a href="#">Em andamento</a></li>
     </ul>
 </div>
-<section class="configuracoes-conta">
-  <h3>Configurações da conta</h3>
+<div class="confi-conta">
+    <h3> Configurações da conta </h3>
+    <ul>
+        <ul>
+  <li>
+    <a href="#">
+      Editar dados
+      <img src="assets/icons/icon-editar(verde).svg" alt="Editar" class="icone">
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      Excluir conta
+      <img src="assets/icons/icon-lixeira(verde).svg" alt="Excluir" class="icone">
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      Sair
+      <img src="assets/icons/icon-logout.svg" alt="Sair" class="icone">
+    </a>
+  </li>
+</ul>
 
-  <button class="botao-configuracao">
-    <img src="/chale/public/assets/icon-editar(verde).svg" alt="Editar dados" width="16">
-    Editar dados
-  </button>
 
-  <button class="botao-configuracao">
-    <img src="/chale/public/assets/icon-lixeira(verde).svg" alt="Excluir conta" width="16">
-    Excluir conta
-  </button>
-
-  <button class="botao-configuracao">
-    <img src="/chale/public/assets/icon-logout.svg" alt="Sair" width="16">
-    Sair
-  </button>
-</section>
-
+    </ul>
+</div>
 <?php
 require_once __DIR__ . '/../layouts/footer.php';
-
 ?>
 
 
