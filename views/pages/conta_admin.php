43d27d293935b743e6945b5fe9f283a38630811e
juzynha <julianacaraujo3103@gmail.com>
2025-05-27 09:03:44 -0300

    correção dos nomes dos arquivos

--- views/pages/conta_admin.php
+++ views/pages/conta_admin.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/../layouts/header.php';
 
 <div class="acoes">
     <h3> Ações </h3>
-    <ul>
+    <ul class="lista">
         <li><a herf="#">Consultar preços e promoções<a/><img src="/chale/public/assets/icons/icon-(verde).svg" class="icone"  width="10"   height="10"></li>
         <li><a herf="#">Consultar reservas<a/><img src="/chale/public/assets/icons/icon-(verde).svg" class="icone"  width="10"   height="10"></li>
         <li><a herf="#">Editar calendário<a/><img src="/chale/public/assets/icons/icon-editar(verde).svg" class="icone"  width="10"   height="10"></li>
@@ -25,7 +25,7 @@ require_once __DIR__ . '/../layouts/header.php';
 </div>
 <div class="confi-conta">
     <h3> Configurações da conta </h3>
-    <ul>
+    <ul class="lista">
         <li><a herf="#">Criar outra conta administradora<a/><img src="/chale/public/assets/icons/icon-adicionar.svg" class="icone"  width="10"   height="10"></li>
         <li><a herf="#">Ver contas adiministradoras<a/><img src="/chale/public/assets/icons/icon-administradores.svg" class="icone"  width="10"   height="10"></li>
         <li><a herf="#">Editar dados<a/><img src="/chale/public/assets/icons/icon-editar(verde).svg" class="icone"  width="10"   height="10"></li>
