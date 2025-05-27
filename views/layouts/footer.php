aa8ffd63a54a13c6f5ceffefead1618d594ead63
Wendler Pocarli <julianacaraujo3103@gmail.com>
2025-05-27 11:02:59 -0300

    salvando minhas alterações antes do pull

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,4 +1,5 @@
 <script src="/chale/public/js/script.js"></script>
 <script src="/chale/public/js/reservas.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
 </body>
 </html>
