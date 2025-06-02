44ef94e6117bf3e0e548954269206897ffde8db4
Juliana Cardoso Araujo <julianacaraujo3103@gmail.com>
2025-06-02 17:53:00 -0300

    adicionando o modal de digite_sua_senha.php

---
+++ views/modals/digite_sua_senha.php
@@ -0,0 +1,15 @@
+<div class="sombra-modal" id="digite_sua_senha">
+    <div class="bloco-modal-geral">
+        <div class="modal-header">
+            <h2>Para editar seus dados digite sua senha</h2>
+            <p class="error"></p>
+        </div>
+        <div class="modal-body">
+            <label for="conf_email">Senha:</label>
+            <input type="text" id="senha_editar_dados" name="senha_editar_dados">
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn" id="editar_dados">Continuar</button>
+        </div>
+    </div>
+</div>
