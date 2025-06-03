4832cdb03020d63172beeb942d78760e9f0bc57c
Wendler Pocarli <julianacaraujo3103@gmail.com>
2025-06-03 20:36:10 -0300

    pag inicial

--- views/pages/inicial.php
+++ views/pages/inicial.php
@@ -14,11 +14,18 @@ require_once __DIR__ . '/../layouts/header.php';
     </div>
 </header>
 <div class="element">
-    <img src="/chale/public/assets/elementos-graficos/chale-transparente.png" class="wave">
+    <img src="/chale/public/assets/elementos-graficos/imgchale-transparente.png" class="wave">
     <!--<img src="/chale/public/assets/elementos-graficos/chale-transparente-inicial-pginicial.png" class="chale-transparente1">-->
     <h2 class="texto-de-entrada">Conforto e Qualidade</h2>
     <h4 class="textinho1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing</h4>
 </div>
+<div> <img src="/chale/public/assets/elementos-graficos/chale-front-transparente.png" class="chale-right">
+<h3 class="textinho2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing</h3>
+</div>
+<div> <img src="/chale/public/assets/elementos-graficos/chale-de-ladinho.png" class="chale-left">
+<h3 class="textinho3">Acima da cama, uma abertura no teto revela o céu estrelado, permitindo que você adormeça sobre um espetáculo de estrelas e acorde com a luz suave de manhã.</h3>
+</div>
+
 
 <?php
 require_once __DIR__ . '/../layouts/footer.php';
