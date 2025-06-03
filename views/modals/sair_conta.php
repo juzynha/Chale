addf0df392544f099b04a5d415e171bbf649f29b
unknown <fefe.ormenese@gmail.com>
2025-06-03 12:21:49 -0300

    Pequenas alteraçoes,modal_sair e conta_admin

--- views/modals/sair_conta.php
+++ views/modals/sair_conta.php
@@ -1,20 +1,13 @@
 
-<div class="sombra-modal" id="sair">
+<div class="sombra-modal" id="modal_sair">
     <div class="bloco-modal-usuario">
         <div class="contorno-modal">
             <div class="modal-content">
-                <div class="modal-header">
-                    <img src="/chale/public/assets/logo.svg" width="120px">
-                    <button onclick="fecharModal('sair')">
-                        <img src="/chale/public/assets/icons/icon-close.svg" width="20px">
-                    </button>
-                </div>
-                <div id="modal-sair" class="modal-sair">
-  <div class="modal-content-sair">
-    <p>Tem certeza que deseja sair?</p>
-    <div class="botoes-modal">
-      <button id="confirmar-sair" class="btn-sair">Sim, sair</button>
-      <button id="cancelar-sair" class="btn-cancelar">Cancelar</button>
+                  
+    <div class="modal-content-sair">
+      <p>Tem certeza que deseja sair?</p>
+      <button class="btn" id="btn_sair">Sim</button>
+      <button class="btn" id="btn_cancelar"onclick="fecharModal('modal_sair')">Não</button>
     </div>
   </div>
 </div>
