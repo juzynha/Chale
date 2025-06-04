db641d21470435cfa3f26017d21fe009e6d61a42
Lilith Pocarli <julianacaraujo3103@gmail.com>
2025-06-04 20:27:13 -0300

    pagina inicial quase finalizada

--- views/pages/inicial.php
+++ views/pages/inicial.php
@@ -17,14 +17,23 @@ require_once __DIR__ . '/../layouts/header.php';
     <img src="/chale/public/assets/elementos-graficos/imgchale-transparente.png" class="wave">
     <!--<img src="/chale/public/assets/elementos-graficos/chale-transparente-inicial-pginicial.png" class="chale-transparente1">-->
     <h2 class="texto-de-entrada">Conforto e Qualidade</h2>
-    <h4 class="textinho1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing</h4>
+    <h4 class="textinho1">Acima da cama, uma abertura no teto revela o céu estrelado, permitindo que você adormeça sobre um espetáculo de estrelas e acorde com a luz suave de manhã.</h4>
 </div>
+<h3 class="textinho2">Acima da cama, uma abertura no teto revela o céu estrelado, permitindo que você adormeça sobre um espetáculo de estrelas e acorde com a luz suave de manhã.</h3>
 <div> <img src="/chale/public/assets/elementos-graficos/chale-front-transparente.png" class="chale-right">
-<h3 class="textinho2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing</h3>
+</div> 
+<div> <img src="/chale/public/assets/elementos-graficos/plaquinha.svg" class="plaquinha">
 </div>
-<div> <img src="/chale/public/assets/elementos-graficos/chale-de-ladinho.png" class="chale-left">
-<h3 class="textinho3">Acima da cama, uma abertura no teto revela o céu estrelado, permitindo que você adormeça sobre um espetáculo de estrelas e acorde com a luz suave de manhã.</h3>
+<h3 class="textinho3">Luzes agradaveis que iluminam o ambiente e trazem uma sensação de conforto e relaxamento.</h3>
 </div>
+<div> <img src="/chale/public/assets/elementos-graficos/chale-de-ladinho.png" class="chale-left"></div>
+<div><img src="/chale/public/assets/elementos-graficos/fogueirinha.svg" class="fogueirinha"></div>
+<h3 class="textinho4">Hidromassagem ao ar livre com barreira anti-vento para banhos confortaveis mesmo a noite.</h3>
+<div><img src="/chale/public/assets/elementos-graficos/chale-espuma.png" class="chale-espuma"></div>
+<div><img src="/chale/public/assets/elementos-graficos/patinhas.svg" class="patinhas"></div>
+<div class="generico">aaaaaaaaaaaaa</div>
+
+
 
 
 <?php
