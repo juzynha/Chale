c94a8a37561ca10c680fc25ee4cc312203a293a5
karol <karolrodriguesrosa.2008@gmail.com>
2025-08-19 12:02:10 -0300

    fiz o modal de méodo de pagamento

---
+++ public/js/reservas.js
@@ -0,0 +1,111 @@
+document.addEventListener("DOMContentLoaded", function() {
+    listaReservas();
+    mostrarReservas();
+});
+
+function listaReservas() {
+    const caminho = '/chale/app/';
+    lista = document.getElementById('lista_reservas');
+    fetch(`${caminho}Models/ReservasModel.php?action=listaReservas`)
+        .then(response => response.json())
+        .then(data => {
+            lista.innerHTML = '';
+            data.forEach(reserva => {
+                let fotoHTML = '';
+                if (reserva.usufoto) {
+                    // Se tiver foto, exibe com uma URL dinâmica (exemplo com exibir_imagem.php?id=)
+                    //fotoHTML = `<img src="${caminho}exibir_imagem.php?tabela=usuarios&campo=usufoto&id=${reserva.usuid}" alt="Foto do usuário" class="foto-usuario">`;
+                } else {
+                    // Se não tiver foto, exibe o ícone padrão
+                    fotoHTML = `<img src="/chale/public/assets/icons/icon-user.svg" width="25px">`;
+                }
+                lista.innerHTML += `
+                    <div class="card-reserva">
+                        <div class="infos-cliente">
+                            <div class="nome-cliente">
+                                <div class="foto-cliente">${fotoHTML}</div>
+                                <p>${reserva.usunome}</p>
+                            </div>
+                            <div class="info">
+                                <h3>Telefone: </h3>
+                                <p>${reserva.usutelefone}</p>
+                            </div>
+                            <div class="info">
+                                <h3>E-mail: </h3>
+                                <p>${reserva.usuemail}</p>
+                            </div>
+                            <div class="info">
+                                <h3>Idade: </h3>
+                                <p>${reserva.idade}</p>
+                            </div>
+                        </div>
+                        <div class="infos-reserva">
+                            <div class="info">
+                                <h3>Valor pago: </h3>
+                                <p>${reserva.resvtotal}</p>
+                            </div>
+                            <div class="info">
+                                <h3>Check-in: </h3>
+                                <p>${reserva.rescheckin}</p>
+                            </div>
+                            <div class="info">
+                                <h3>Check-out: </h3>
+                                <p>${reserva.rescheckout}</p>
+                            </div>
+                        </div>
+                        <div class="excluir-reserva">
+                            <img src="/chale/public/assets/icons/icon-lixeira.svg" class="icon" onclick="abrirModal('excluir_reserva')">
+                        </div>
+                    </div>
+                    `;
+            });
+        });
+}
+
+function mostrarReservas() {
+    let mostrar = document.getElementById('mostrar');
+
+    mostrar.addEventListener("click", function () {
+        let container = document.getElementById('container_reservas');
+
+        if (mostrar.textContent === 'Mostrar tudo') {
+            // Ação: expandir
+            container.classList.remove("limita-tamanho");
+            container.classList.add("container-reservas");
+            mostrar.textContent = 'Mostrar menos';
+        } else {
+            // Ação: recolher
+            container.classList.remove("container-reservas");
+            container.classList.add("limita-tamanho");
+            mostrar.textContent = 'Mostrar tudo';
+        }
+    });
+}
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    const formLogin = document.getElementById("formLogin");
+    const errorElement = formLogin.querySelector(".error");
+
+    formLogin.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(formLogin);
+
+        fetch("/chale/app/Controllers/UsuarioController.php", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.text())
+        .then(result => {
+            if (result.trim() === "ok") {
+                window.location.href = "/chale/index.php";
+            } else {
+                errorElement.textContent = result;
+            }
+        })
+        .catch(() => {
+            errorElement.textContent = "Erro ao tentar logar. Tente novamente.";
+        });
+    });
+});
+*/
---
+++ views/modals/cadastro_usuario.php
@@ -0,0 +1,88 @@
+<div class="sombra-modal" id="cadastro">
+    <div class="bloco-modal-usuario">
+        <div class="contorno-modal">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <img src="/chale/public/assets/logo.svg" width="120px">
+                    <button onclick="fecharModal('cadastro')">
+                        <img src="/chale/public/assets/icons/icon-close.svg" width="20px">
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h2>Já possui uma conta? Fazer <p class="link" id="abrir_login">login</p>
+                    </h2>
+                    <form id="formCadastro" action="/chale/app/controller/usuario.php" method="post">
+                        <p class="mensagem-erro"></p>
+                        <div class="input-wrapper">
+                            <label for="nome">Nome:</label>
+                            <input type="text" id="nome" name="nome" required>
+                        </div>
+                        <div class="input-wrapper">
+                            <label for="email">E-mail:</label>
+                            <input type="email" id="email" name="email" required>
+                        </div>
+                        <div class="input-wrapper">
+                            <label for="telefone">Telefone:</label>
+                            <input type="text" id="telefone" name="telefone" required>
+                        </div>
+                        <div class="input-wrapper">
+                            <label for="data_nasc">Data de nascimento:</label>
+                            <input type="text" id="data_nasc" name="data_nasc" required>
+                        </div>
+                        <div class="input-wrapper">
+                            <label for="senha">Senha:</label>
+                            <input type="password" id="senha" name="senha" required>
+                        </div>
+                        <div class="last-input">
+                            <div class="input-wrapper">
+                                <label for="senha">Confirmar senha:</label>
+                                <input type="password" id="conf_senha" name="conf_senha" required>
+                            </div>
+                            <p id="esqueci_senha" class="textinho">Esqueci a senha</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn" id="continuar_cadastro">Continuar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="sombra-modal" id="confirmar_email">
+    <div class="bloco-modal-geral">
+        <div class="modal-header">
+            <h2>Enviamos um código ao e-mail <p>gabriel@gmail</p></h2>
+        </div>
+        <div class="modal-body">
+            <label for="conf_email">Digite o código:</label>
+            <input type="text" id="conf_email" name="conf_email" required>
+            <p id="reenviar_email" class="textinho">Reenviar</p>
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn" id="continuar_email">Continuar</button>
+        </div>
+    </div>
+</div>
+
+<div class="sombra-modal" id="colocar_foto">
+    <div class="bloco-modal-geral">
+        <div class="modal-header">
+            <h2>Escolha uma foto de perfil</h2>
+        </div>
+        <div class="modal-body">
+            <div class="modal-centralizado">
+                <div class="foto-user">
+                    <img src="/chale/public/assets/icons/icon-user.svg" width="100px">
+                </div>
+            </div>
+            <label for="foto">Abrir arquivo:</label>
+            <input type="file" id="foto" name="foto" required>
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn" id="continuar_email">Salvar</button>
+            <p class="textinho">Pular <img src="/chale/public/assets/icons/icon-seta-pular.svg" width="12px"></p>
+        </div>
+    </div>
+</div>
