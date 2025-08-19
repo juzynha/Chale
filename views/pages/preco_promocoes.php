9c6f1c4c1c5cea2375943418ac5b5734f96bd0af
ma-fefe <fefe.ormenese@gmail.com>
2025-08-19 10:35:28 -0300

    pagina preço e promoçao

---
+++ views/pages/preco_promocoes.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/../layouts/header.php';
+?>
+
+<body data-page="reservas">
+    <header class="cabecalho">
+        <?php
+    require_once __DIR__ . '/../layouts/menu.php';
+    ?>
+    </header>
+    <section class="container-reservas" id="container_reservas">
+        <h2 class="subtitulo verde-escuro">Reservas</h2>
+        <div class="filtro">
+            <input type="text" id="data_filtro">
+            <img src="/chale/public/assets/icons/icon-funil.svg" width="20px">
+        </div>
