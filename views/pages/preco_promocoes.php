6b9c560376cc45c20a8c8958800fddd63e477b02
ma-fefe <fefe.ormenese@gmail.com>
2025-08-19 10:43:34 -0300

    pagina de preço e promoçao

--- views/pages/preco_promocoes.php
+++ views/pages/preco_promocoes.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../layouts/header.php';
     ?>
     </header>
     <section class="container-reservas" id="container_reservas">
-        <h2 class="subtitulo verde-escuro">Reservas</h2>
+        <h2 class="subtitulo verde-escuro">Preços</h2>
         <div class="filtro">
             <input type="text" id="data_filtro">
             <img src="/chale/public/assets/icons/icon-funil.svg" width="20px">
