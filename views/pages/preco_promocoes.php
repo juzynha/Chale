118452ebd058ba321e59272a23ab02e4c9075cbc
ma-fefe <fefe.ormenese@gmail.com>
2025-08-19 11:55:21 -0300

    pagina preço e promoção

--- views/pages/preco_promocoes.php
+++ views/pages/preco_promocoes.php
@@ -8,9 +8,36 @@ require_once __DIR__ . '/../layouts/header.php';
     require_once __DIR__ . '/../layouts/menu.php';
     ?>
     </header>
-    <section class="container-reservas" id="container_reservas">
+    <section class="container-precos" id="container_precos">
         <h2 class="subtitulo verde-escuro">Preços</h2>
         <div class="filtro">
+            
             <input type="text" id="data_filtro">
             <img src="/chale/public/assets/icons/icon-funil.svg" width="20px">
         </div>
+        
+<section class="container-fr">    
+                 
+    <div class="container-bloco-preco">
+        
+        <div class="card-infos-fr" id="preFormReserva">
+            
+            <p>Diária: R$<strong name="preco_diaria"></strong></p>
+            <p>Diária fim de semana: R$<strong name="preco_diaria_fds"></strong></p>
+
+        </div>
+        
+    </div>
+    <img src="/chale/public/assets/icons/icon-editar(verde).svg" class="lapzinho">
+</section>
+
+
+        <hr>
+
+    
+            
+</body>
+
+<?php
+require_once __DIR__ . '/../layouts/footer.php';
+?>
